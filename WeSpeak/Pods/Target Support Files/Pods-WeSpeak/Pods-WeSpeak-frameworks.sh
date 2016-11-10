6901e902cc5495d3f932f3591231597586cf02ef
Girgez <datlt.uit@gmail.com>
2016-11-10 22:13:43 +0700

    add cosmos

--- WeSpeak/Pods/Target Support Files/Pods-WeSpeak/Pods-WeSpeak-frameworks.sh
+++ WeSpeak/Pods/Target Support Files/Pods-WeSpeak/Pods-WeSpeak-frameworks.sh
@@ -85,11 +85,13 @@ strip_invalid_archs() {
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "$BUILT_PRODUCTS_DIR/AFNetworking/AFNetworking.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/Cosmos/Cosmos.framework"
   install_framework "$BUILT_PRODUCTS_DIR/MBProgressHUD/MBProgressHUD.framework"
   install_framework "$BUILT_PRODUCTS_DIR/Realm/Realm.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "$BUILT_PRODUCTS_DIR/AFNetworking/AFNetworking.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/Cosmos/Cosmos.framework"
   install_framework "$BUILT_PRODUCTS_DIR/MBProgressHUD/MBProgressHUD.framework"
   install_framework "$BUILT_PRODUCTS_DIR/Realm/Realm.framework"
 fi
